5bc1d45d0080ce96482a115441b2a5b6aa4db793
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-07-19 19:06:22 +0530

    updated changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -88,7 +88,7 @@ app.use(helmet({
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
